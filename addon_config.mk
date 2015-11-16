046e3f1b8c40c79249f5c076d088c764b8fa1f51
Christopher Baker <me@christopherbaker.net>
2015-11-16 00:41:31 -0600

    Add addon_config.mk and thumbnail.

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxNetworkUtils
+	ADDON_DESCRIPTION = A set of network utilities
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "network" "utilities" "CIDR"
+	ADDON_URL = http://github.com/bakercp/ofxNetworkUtils
