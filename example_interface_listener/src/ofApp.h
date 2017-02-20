5b30c8980e9d4291b77e028413bd477cbaea5f24
Christopher Baker <me@christopherbaker.net>
2017-02-20 00:33:20 -0600

    Add interface listener example.

---
+++ example_interface_listener/src/ofApp.h
@@ -0,0 +1,29 @@
+//
+// Copyright (c) 2014 Christopher Baker <https://christopherbaker.net>
+//
+// SPDX-License-Identifier:	MIT
+//
+
+
+#pragma once
+
+
+#include "ofMain.h"
+#include "ofxNetworkUtils.h"
+
+
+class ofApp: public ofBaseApp
+{
+public:
+    void setup();
+    void draw();
+
+    void onInterfaceUp(const Poco::Net::NetworkInterface& interface);
+    void onInterfaceDown(const Poco::Net::NetworkInterface& interface);
+
+    ofxNet::NetworkInterfaceListener networkInterfaceListener;
+
+    ofEventListener interfaceUpListener;
+    ofEventListener interfaceDownListener;
+
+};
