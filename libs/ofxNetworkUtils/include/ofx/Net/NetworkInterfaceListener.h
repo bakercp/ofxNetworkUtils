42b1136e142395418ebcc30dd38c1e51bab4ce6b
Christopher Baker <me@christopherbaker.net>
2017-02-20 00:32:24 -0600

    Add network listener.

--- libs/ofxNetworkUtils/include/ofx/Net/NetworkInterfaceListener.h
+++ libs/ofxNetworkUtils/include/ofx/Net/NetworkInterfaceListener.h
@@ -4,53 +4,49 @@
 // SPDX-License-Identifier:	MIT
 //
 //
-//
-//#pragma once
-//
-//
-//#include "ofEvents.h"
-//
-//
-//namespace ofx {
-//namespace Utils {
-//
-//
-//class NetworkInterfaceEventArgs: public ofEventArgs
-//{
-//public:
-//    NetworkInterfaceEventArgs(NetworkInterface& interface): _interface(interface))
-//    {
-//    }
-//
-//    NetworkInterface& getInterfaceRef()
-//    {
-//        return _interface;
-//    }
-//
-//private:
-//
-//    NetworkInterface& _interface;
-//};
-//
-//
-//class NetworkInterfaceEvents
-//{
-//public:
-//    ofEvent<NetworkInterfaceEventArgs> onInterfaceAdded;
-//    ofEvent<NetworkInterfaceEventArgs> onInterfaceRemoved;
-//    ofEvent<NetworkInterfaceEventArgs> onInterfaceChanged;
-//
-//};
-//
-//
-//class NetworkInterfaceListener
-//{
-//public:
-//    NetworkInterfaceListener();
-//    ~NetworkInterfaceListener();
-//
-//};
-//
-//
-//
-//} } // ofx::Utils
+
+#pragma once
+
+
+#include "ofEvents.h"
+#include "Poco/Net/NetworkInterface.h"
+
+
+namespace ofx {
+namespace Net {
+
+
+class NetworkInterfaceListener
+{
+public:
+    NetworkInterfaceListener();
+    ~NetworkInterfaceListener();
+
+    void update(ofEventArgs& args);
+
+    /// \returns the last known interface map.
+    const Poco::Net::NetworkInterface::Map& interfaces() const;
+
+    ofEvent<const Poco::Net::NetworkInterface> onInterfaceUp;
+    ofEvent<const Poco::Net::NetworkInterface> onInterfaceDown;
+
+    enum
+    {
+        /// \brief The default polling interval in milliseconds.
+        DEFAULT_POLL_INTERVAL = 1000
+    };
+
+
+private:
+    ofEventListener _updateListener;
+
+    uint64_t _lastUpdate = 0;
+    uint64_t _updateInterval = DEFAULT_POLL_INTERVAL;
+
+    Poco::Net::NetworkInterface::Map _interfaces;
+
+};
+
+
+
+} } // ofx::Utils
