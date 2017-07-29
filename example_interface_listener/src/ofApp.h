c5ab71255d5533dabadd1a799205ba380bcb14cd
Christopher Baker <me@christopherbaker.net>
2017-07-29 12:25:10 -0500

    Modernize.

--- example_interface_listener/src/ofApp.h
+++ example_interface_listener/src/ofApp.h
@@ -15,8 +15,8 @@
 class ofApp: public ofBaseApp
 {
 public:
-    void setup();
-    void draw();
+    void setup() override;
+    void draw() override;
 
     void onInterfaceUp(const Poco::Net::NetworkInterface& interface);
     void onInterfaceDown(const Poco::Net::NetworkInterface& interface);
